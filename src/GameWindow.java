14921dd815917aac1c4ec28bdb4a535ff67f6d69
Jay Bishop <jaycgtsp@gmail.com>
2016-02-23 19:07:55 -0700

    Initial - Grid
    Grid Finished with Buttons instead of Tiles

---
+++ src/GameWindow.java
@@ -0,0 +1,106 @@
+/**
+ * @author Kim Buckner
+ * Date: Feb 19, 2016
+ *
+ * This is the actual "game". May/will have to make some major changes.
+ * This is just a "hollow" shell.
+ *
+ * When you get done, I should see the buttons at the top in the "play" area
+ * (not a pull-down menu). The only one that should do anything is Quit.
+ *
+ * Should also see something that shows where the 4x4 board and the "spare"
+ * tiles will be when we get them stuffed in.
+ */
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameWindow extends JFrame implements ActionListener
+  {
+    /**
+     * because it is a serializable object, need this or javac
+     * complains a lot
+     */
+    public static final long serialVersionUID=1;
+
+    /*
+     * Here I declare some buttons and declare an array to hold the grid elements. 
+     * But, you can do what you want.
+     */
+    GridButton grid[] = new GridButton[16];
+    
+    private int startAt=1;
+    
+    /**
+     * Constructor sets the window name using super(), changes the layout,
+     * which you really need to read up on, and maybe you can see why I chose
+     * this one.
+     *
+     * @param s
+     */
+
+    public GameWindow(String s)
+    {
+      super(s);
+      GridBagLayout gbl=new GridBagLayout();
+      setLayout(gbl);
+    }
+
+    /**
+     * For the buttons
+     * @param e is the ActionEvent
+     * 
+     * BTW can ask the event for the name of the object generating event.
+     * The odd syntax for non-java people is that "exit" for instance is
+     * converted to a String object, then that object's equals() method is
+     * called.
+     */
+
+    public void actionPerformed(ActionEvent e) {
+      if("exit".equals(e.getActionCommand()))
+        System.exit(0);
+      if("reset".equals(e.getActionCommand()))
+        System.out.println("reset pressed\n");
+      if("new".equals(e.getActionCommand()))
+        System.out.println("new pressed\n");
+      }
+
+    /**
+     *  Establishes the initial board
+     */
+
+    public void setUp()
+    {
+      //actually create the array for elements, make sure it is big enough
+      // Need to play around with the dimensions and the gridx/y values
+      // These constraints are going to be added to the pieces/parts I 
+      // stuff into the "GridBag".
+    	
+      GridBagConstraints basic = new GridBagConstraints();
+
+      //Here I create 16 elements to put into my gameBoard and add them
+      for(int i = 0; i < 16; i++)
+      {
+    	  grid[i] = new GridButton(this, i);
+    	  Insets inset = grid[i].inset;
+    	  this.addButtons(this,grid[i].button,(i%4),(i/4),1,1,basic.CENTER,basic.BOTH,inset);
+      }
+      
+      return;
+    }
+    /**
+     * Used by setUp() to configure the buttons on a button bar and
+     * add it to the gameBoard
+     */
+
+    public void addButtons(Container container, Component component, int gridx, int gridy,
+    	      int gridwidth, int gridheight, int anchor, int fill, Insets in){
+    	GridBagConstraints gbc = new GridBagConstraints
+    			(gridx, gridy, gridwidth, gridheight, 1.0, 1.0,
+    	        anchor, fill, in, 0, 0);
+    	    container.add(component, gbc);
+        return;
+    }
+
+  };
---
+++ src/GridButton.java
@@ -0,0 +1,53 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GridButton {
+	public final JButton button = new JButton();
+	public Insets inset;
+	
+	GridButton(JFrame frame,int i)
+	{
+		int width = frame.getWidth();
+	    int height = frame.getHeight();
+	    
+	    switch (i)
+	    {
+	    // Center Grid Pieces
+	    case 5:
+	    case 6:
+	    case 9:
+	    case 10: inset = new Insets(0,0,0,0);
+	        break;
+	    // Top Grid Pieces
+	    case 1:
+	    case 2: inset = new Insets(height/3,0,0,0);
+	        break;
+	    // Bottom Grid Pieces
+	    case 13:
+	    case 14: inset = new Insets(0,0,height/3,0);
+	        break;
+	    // Left Grid Pieces
+	    case 4: 
+	    case 8: inset = new Insets(0,width/3,0,0); 
+	        break;
+	    // Right Grid Pieces
+	    case 7:
+	    case 11: inset = new Insets(0,0,0,width/3);
+	        break;
+	    // TopLeft Piece
+	    case 0: inset = new Insets(height/3,width/3,0,0);
+            break;
+	    // TopRight Piece
+	    case 3: inset = new Insets(height/3,0,0,width/3);
+	    	break;
+	    // BotLeft Piece
+	    case 12: inset = new Insets(0,width/3,height/3,0);
+	    	break;
+	    // BotRight Piece
+	    case 15: inset = new Insets(0,0,height/3,width/3);
+	    break;  
+	    }
+	}
+}
