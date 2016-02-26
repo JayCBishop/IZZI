154063bdb6fa17b0fd99f1bc8bb7303bd5741fa6
dkretzsc <dkretzsc@uwyo.edu>
2016-02-25 17:41:32 -0700

    Add abstract class TileArea
    Stub

---
+++ src/TileArea.java
@@ -0,0 +1,27 @@
+package program01;
+import java.awt.*;
+
+public abstract class TileArea {
+  private Tile tile;
+  private boolean blank;
+  /**
+   * Stupid serializable stuff
+   */
+  private static final long serialVersionUID = 2L;
+  
+  public TileArea(){
+    //stub for constructor
+  }
+  
+  public boolean isSpaceBlank(){
+    return blank;
+    
+  }
+  
+  public void getTile(){
+    //stub
+  }
+  public void placeTile(){
+    //stub
+  }
+}
